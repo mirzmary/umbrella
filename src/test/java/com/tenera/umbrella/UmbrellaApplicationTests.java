2893471444d421857338759b200b01c62bf61ed4
Mary Mirzoyan <mary.mirzoyan@zenjob.com>
2021-05-14 11:15:41 +0100

    first commit

---
+++ src/test/java/com/tenera/umbrella/UmbrellaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tenera.umbrella;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UmbrellaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
